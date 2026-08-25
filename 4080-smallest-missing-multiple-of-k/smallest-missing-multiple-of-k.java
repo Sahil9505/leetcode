b2b8f0935a4ae3e0308fe03ab8945f29c90eb1ad
Sahil Sharma <sahilsharma9505@gmail.com>
2026-08-25 14:06:46 +0530

    Time: 1 ms (82.00%) | Memory: 44.9 MB (83.31%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,15 +1,13 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            seen.add(num);
-        }
-
-        int cur = k;
-        while (seen.contains(cur)) {
-            cur += k;
-        }
-
-        return cur;
+        long[] x = {0L, 0L};
+        for (int n : nums)
+            if (n % k == 0) {
+                int i = n / k - 1;
+                x[i >> 6] |= 1L << (i & 63);
+            }
+            
+        int z = x[0] == -1L ? 1 : 0;
+        return (z * 64 + Long.numberOfTrailingZeros(++x[z] & -x[z]) + 1) * k;
     }
 }
